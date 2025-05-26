2520d9e5f140181b0acb3d04d1e8db8d46932d62
Ahmed Adel <aadel@tanfeethi.com.sa>
2025-05-26 11:06:14 +0300

    make design our services screen

---
+++ lib/features/user/home/presentation/screens/our_services.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:truee_balance_app/core/themes/app_colors.dart';
+import 'package:truee_balance_app/core/widgets/app_bar/custom_app_bar_widget.dart';
+import 'package:truee_balance_app/features/user/home/presentation/widgets/services_grid_widget.dart';
+
+class OurServicesScreen extends StatelessWidget {
+  const OurServicesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor900,
+      appBar: CustomBasicAppBar(
+        leading: BackButton(
+          color: AppColors.neutralColor100,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: 'Our Services',
+        backgroundColor: AppColors.primaryColor900,
+        svgAsset: 'assets/images/svg/bg_image.svg',
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(18.sp),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(12.r),
+            topRight: Radius.circular(12.r),
+          ),
+        ),
+        child: ServicesGridWidget(),
+      ),
+    );
+  }
+}
