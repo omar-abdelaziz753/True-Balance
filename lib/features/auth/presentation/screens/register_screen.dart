98fb05e64d7bc8787e46a0d35eb03649112dca73
mohamedhisham212213 <mhisham@tanfeethi.com.sa>
2025-06-02 14:35:00 +0300

    make login and register

--- lib/features/auth/presentation/screens/register_screen.dart
+++ lib/features/auth/presentation/screens/register_screen.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:truee_balance_app/core/themes/app_colors.dart';
+import 'package:truee_balance_app/core/utils/easy_loading.dart';
 import 'package:truee_balance_app/features/auth/presentation/widgets/custom_header_widget.dart';
 import 'package:truee_balance_app/features/auth/presentation/widgets/register_form_widget.dart';
 
@@ -9,30 +10,27 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    hideLoading();
     return Scaffold(
       backgroundColor: AppColors.primaryColor900,
       body: SafeArea(
         child: LayoutBuilder(
           builder: (context, constraints) {
-            return SingleChildScrollView(
-              physics: const NeverScrollableScrollPhysics(),
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  minHeight: constraints.maxHeight,
-                ),
-                child: IntrinsicHeight(
-                  child: Center(
-                    child: Column(
-                      children: [
-                        CustomHeaderWidget(
-                          title1: 'welcomeTo'.tr(),
-                          title2: 'trueBalance'.tr(),
-                          description:
-                              'descriptionOfHeaderInRegister'.tr(),
-                        ),
-                        const Expanded(child: ResisterFormWidget()),
-                      ],
-                    ),
+            return ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+              ),
+              child: IntrinsicHeight(
+                child: Center(
+                  child: Column(
+                    children: [
+                      CustomHeaderWidget(
+                        title1: 'welcomeTo'.tr(),
+                        title2: 'trueBalance'.tr(),
+                        description: 'descriptionOfHeaderInRegister'.tr(),
+                      ),
+                      const Expanded(child: ResisterFormWidget()),
+                    ],
                   ),
                 ),
               ),
--- lib/features/auth/presentation/widgets/register_form_widget.dart
+++ lib/features/auth/presentation/widgets/register_form_widget.dart
@@ -1,151 +1,202 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:truee_balance_app/core/extensions/navigation_extension.dart';
-import 'package:truee_balance_app/core/routing/routes_name.dart';
 import 'package:truee_balance_app/core/themes/app_colors.dart';
 import 'package:truee_balance_app/core/themes/text_colors.dart';
 import 'package:truee_balance_app/core/widgets/button/custom_button_widget.dart';
 import 'package:truee_balance_app/core/widgets/text_field/custom_text_form_field_widget.dart';
+import 'package:truee_balance_app/features/auth/business_logic/auth_cubit.dart';
+import 'package:truee_balance_app/features/auth/presentation/screens/verify_otp_screen.dart';
 
 class ResisterFormWidget extends StatelessWidget {
   const ResisterFormWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: EdgeInsets.symmetric(
-        horizontal: 18.w,
-        vertical: 23.h,
-      ),
-      decoration: BoxDecoration(
-        color: AppColors.neutralColor100,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(12.r),
-          topRight: Radius.circular(12.r),
-        ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'email'.tr(),
-            style: Styles.contentEmphasis.copyWith(
-              color: AppColors.neutralColor1000,
+    final cubit = context.read<AuthCubit>();
+    return SingleChildScrollView(
+      child: Form(
+        key: cubit.formKey,
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 18.w, vertical: 23.h),
+          decoration: BoxDecoration(
+            color: AppColors.neutralColor100,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12.r),
+              topRight: Radius.circular(12.r),
             ),
           ),
-          8.verticalSpace,
-          CustomTextFormFieldWidget(
-            controller: TextEditingController(),
-            keyboardType: TextInputType.emailAddress,
-            hintText: 'enterYourEmail'.tr(),
-            hintStyle: Styles.captionRegular.copyWith(
-              color: AppColors.neutralColor600,
-            ),
-            borderColor: AppColors.neutralColor600,
-          ),
-          18.verticalSpace,
-          Text(
-            'phoneNumberOnly'.tr(),
-            style: Styles.contentEmphasis.copyWith(
-              color: AppColors.neutralColor1000,
-            ),
-          ),
-          8.verticalSpace,
-          CustomTextFormFieldWidget(
-            controller: TextEditingController(),
-            keyboardType: TextInputType.emailAddress,
-            hintText: 'enterYourPhoneNumber'.tr(),
-            hintStyle: Styles.captionRegular.copyWith(
-              color: AppColors.neutralColor600,
-            ),
-            borderColor: AppColors.neutralColor600,
-          ),
-          18.verticalSpace,
-          Text(
-            'password'.tr(),
-            style: Styles.contentEmphasis.copyWith(
-              color: AppColors.neutralColor1000,
-            ),
-          ),
-          8.verticalSpace,
-          CustomTextFormFieldWidget(
-            controller: TextEditingController(),
-            keyboardType: TextInputType.emailAddress,
-            hintText: 'enterYourPassword'.tr(),
-            hintStyle: Styles.captionRegular.copyWith(
-              color: AppColors.neutralColor600,
-            ),
-            borderColor: AppColors.neutralColor600,
-            suffixIcon: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Iconsax.eye,
-                color: AppColors.neutralColor600,
-              ),
-            ),
-          ),
-          32.verticalSpace,
-          CustomButtonWidget(
-            text: 'next'.tr(),
-            padding: EdgeInsets.symmetric(
-              vertical: 14.h,
-            ),
-            textStyle: Styles.captionEmphasis.copyWith(
-              color: AppColors.neutralColor100,
-            ),
-            onPressed: () => context.pushNamed(
-              Routes.verifyOtpScreen,
-              arguments: {
-                'email': 'Omar@gmail.com',
-                'screenName': 'Register',
-              },
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                'alreadyHaveAccount',
-                style: Styles.captionEmphasis.copyWith(
-                  color: AppColors.neutralColor300,
+              Text('name'.tr(),
+                  style: Styles.contentEmphasis
+                      .copyWith(color: AppColors.neutralColor1000)),
+              8.verticalSpace,
+              CustomTextFormFieldWidget(
+                controller: cubit.nameController,
+                keyboardType: TextInputType.text,
+                hintText: 'enterYourName'.tr(),
+                hintStyle: Styles.captionRegular
+                    .copyWith(color: AppColors.neutralColor600),
+                borderColor: AppColors.neutralColor600,
+                suffixIcon: IconButton(
+                  onPressed: cubit.toggleObscure,
+                  icon: Icon(Iconsax.eye, color: AppColors.neutralColor600),
+                ),
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'nameRequired'.tr();
+                  } else if (value.trim().length < 2) {
+                    return 'nameTooShort'.tr();
+                  }
+                  return null;
+                },
+              ),
+              18.verticalSpace,
+              Text('email'.tr(),
+                  style: Styles.contentEmphasis
+                      .copyWith(color: AppColors.neutralColor1000)),
+              8.verticalSpace,
+              CustomTextFormFieldWidget(
+                controller: cubit.emailController,
+                keyboardType: TextInputType.emailAddress,
+                hintText: 'enterYourEmail'.tr(),
+                hintStyle: Styles.captionRegular
+                    .copyWith(color: AppColors.neutralColor600),
+                borderColor: AppColors.neutralColor600,
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'emailRequired'.tr();
+                  } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                      .hasMatch(value.trim())) {
+                    return 'emailInvalid'.tr();
+                  }
+                  return null;
+                },
+              ),
+              18.verticalSpace,
+              Text('phoneNumberOnly'.tr(),
+                  style: Styles.contentEmphasis
+                      .copyWith(color: AppColors.neutralColor1000)),
+              8.verticalSpace,
+              CustomTextFormFieldWidget(
+                controller: cubit.phoneController,
+                keyboardType: TextInputType.phone,
+                hintText: 'enterYourPhoneNumber'.tr(),
+                hintStyle: Styles.captionRegular
+                    .copyWith(color: AppColors.neutralColor600),
+                borderColor: AppColors.neutralColor600,
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'phoneRequired'.tr();
+                  } else if (!RegExp(r'^\d{9,15}$').hasMatch(value.trim())) {
+                    return 'phoneInvalid'.tr();
+                  }
+                  return null;
+                },
+              ),
+              18.verticalSpace,
+              Text('password'.tr(),
+                  style: Styles.contentEmphasis
+                      .copyWith(color: AppColors.neutralColor1000)),
+              8.verticalSpace,
+              CustomTextFormFieldWidget(
+                controller: cubit.passwordController,
+                keyboardType: TextInputType.visiblePassword,
+                hintText: 'enterYourPassword'.tr(),
+                hintStyle: Styles.captionRegular
+                    .copyWith(color: AppColors.neutralColor600),
+                borderColor: AppColors.neutralColor600,
+                suffixIcon: IconButton(
+                  onPressed: cubit.toggleObscure,
+                  icon: Icon(Iconsax.eye, color: AppColors.neutralColor600),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'passwordRequired'.tr();
+                  } else if (value.length < 6) {
+                    return 'passwordTooShort'.tr();
+                  }
+                  return null;
+                },
+              ),
+              32.verticalSpace,
+              BlocListener<AuthCubit, AuthState>(
+                listener: (context, state) {
+                  if (state is RegisterSuccessState) {
+                    // context.pushNamed(
+                    //   Routes.verifyOtpScreen,
+                    //   arguments: ,
+                    // );
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (BuildContext context) {
+                      return BlocProvider.value(
+                        // create: (context) => SubjectBloc(),
+                        value: cubit,
+                        child: VerifyOtpScreen(data: {
+                          'email': cubit.emailController.text.trim(),
+                          'screenName': 'Register',
+                        }),
+                      );
+                    }));
+                  }
+                },
+                child: CustomButtonWidget(
+                  text: 'next'.tr(),
+                  padding: EdgeInsets.symmetric(vertical: 14.h),
+                  textStyle: Styles.captionEmphasis
+                      .copyWith(color: AppColors.neutralColor100),
+                  onPressed: () {
+                    cubit.verificationCodeController.clear();
+                    if (cubit.formKey.currentState!.validate()) {
+                      cubit.userRegister();
+                    }
+                  },
                 ),
               ),
-              TextButton(
-                onPressed: () => context.pop(),
-                style: TextButton.styleFrom(
-                    padding: EdgeInsets.only(
-                      left: 6.w,
-                      top: 18.w,
-                      bottom: 18.w,
-                      // right: 10.w,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'alreadyHaveAccount',
+                    style: Styles.captionEmphasis
+                        .copyWith(color: AppColors.neutralColor300),
+                  ),
+                  TextButton(
+                    onPressed: () => context.pop(),
+                    style: TextButton.styleFrom(
+                      padding:
+                          EdgeInsets.only(left: 6.w, top: 18.w, bottom: 18.w),
+                      minimumSize: Size.zero,
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      foregroundColor: AppColors.primaryColor200,
                     ),
-                    minimumSize: Size.zero,
-                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                    foregroundColor: AppColors.primaryColor200),
-                child: Row(
-                  children: [
-                    Text(
-                      'login'.tr(),
-                      style: Styles.captionEmphasis.copyWith(
-                        color: AppColors.primaryColor900,
-                        decoration: TextDecoration.underline,
-                        decorationThickness: 1.5.w,
-                        decorationColor: AppColors.primaryColor900,
-                      ),
+                    child: Row(
+                      children: [
+                        Text(
+                          'login'.tr(),
+                          style: Styles.captionEmphasis.copyWith(
+                            color: AppColors.primaryColor900,
+                            decoration: TextDecoration.underline,
+                            decorationThickness: 1.5.w,
+                            decorationColor: AppColors.primaryColor900,
+                          ),
+                        ),
+                        SvgPicture.asset('assets/images/svg/sign_up_icon.svg'),
+                      ],
                     ),
-                    SvgPicture.asset(
-                      'assets/images/svg/sign_up_icon.svg',
-                    )
-                  ],
-                ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
