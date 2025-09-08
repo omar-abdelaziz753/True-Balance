4d5ec77e322bd92b672d32d0c08c62ffca8bfa57
Ahmed Adel <aadel@tanfeethi.com.sa>
2025-09-08 15:36:06 +0300

    down load file

--- lib/core/helper_functions/download_function.dart
+++ lib/core/helper_functions/download_function.dart
@@ -68,328 +68,48 @@
 //     debugPrint("Download failed: $e");
 //   }
 // }
-// import 'dart:io';
-
-// import 'package:dio/dio.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_easyloading/flutter_easyloading.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:path_provider/path_provider.dart';
-// import 'package:truee_balance_app/core/themes/app_colors.dart';
-// import 'package:truee_balance_app/core/utils/app_constants.dart';
-
-// Future<void> downloadPdfFile(String url, String fileName) async {
-//   try {
-//     debugPrint("Downloading from: $url");
-//     Directory? dir;
-//     if (Platform.isAndroid) {
-//       dir = Directory('/storage/emulated/0/Download');
-//       if (!dir.existsSync()) {
-//         dir = await getExternalStorageDirectory();
-//       }
-//     } else {
-//       dir = await getApplicationDocumentsDirectory();
-//     }
-
-//     String savePath = "${dir!.path}/$fileName";
-
-//     Dio dio = Dio();
-//     await dio.download(
-//       url,
-//       savePath,
-//       onReceiveProgress: (received, total) {
-//         EasyLoading.instance
-//           ..indicatorType = EasyLoadingIndicatorType.fadingCircle
-//           ..loadingStyle = EasyLoadingStyle.custom
-//           ..indicatorSize = 50.sp
-//           ..radius = AppConstants.borderRadius
-//           ..backgroundColor = AppColors.neutralColor100
-//           ..indicatorColor = AppColors.primaryColor900
-//           ..textColor = Colors.black
-//           ..maskColor = Colors.black.withValues(alpha: 0.5)
-//           ..progressColor = AppColors.primaryColor900
-//           ..userInteractions = false;
-
-//         if (total != -1) {
-//           double progress = received / total;
-//           EasyLoading.showProgress(
-//             progress,
-//             status: 'Downloading... ${(progress * 100).toStringAsFixed(0)}%',
-//             maskType: EasyLoadingMaskType.clear,
-//           );
-//         }
-//       },
-//     );
-
-//     EasyLoading.dismiss();
-//     EasyLoading.showSuccess("Saved to Downloads");
-//     debugPrint("File downloaded to: $savePath");
-//   } catch (e) {
-//     EasyLoading.dismiss();
-//     EasyLoading.showError("Download failed");
-//     debugPrint("Download failed: $e");
-//   }
-// }
-// import 'dart:io';
-
-// import 'package:dio/dio.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_easyloading/flutter_easyloading.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:path_provider/path_provider.dart';
-// import 'package:permission_handler/permission_handler.dart';
-// import 'package:truee_balance_app/core/themes/app_colors.dart';
-// import 'package:truee_balance_app/core/utils/app_constants.dart';
-
-// Future<void> downloadPdfFile(String url, String fileName) async {
-//   try {
-//     if (Platform.isAndroid) {
-//       var status = await Permission.storage.request();
-//       if (!status.isGranted) {
-//         debugPrint("Storage permission denied");
-//         EasyLoading.showError("Permission denied");
-//         return;
-//       }
-//     }
-
-//     Directory dir = Platform.isAndroid
-//         ? (await getExternalStorageDirectory())!
-//         : await getApplicationDocumentsDirectory();
-
-//     String savePath = "${dir.path}/$fileName";
-
-//     Dio dio = Dio();
-//     await dio.download(
-//       url,
-//       savePath,
-//       onReceiveProgress: (received, total) {
-//         EasyLoading.instance
-//           ..indicatorType = EasyLoadingIndicatorType.fadingCircle
-//           ..loadingStyle = EasyLoadingStyle.custom
-//           ..indicatorSize = 50.sp
-//           ..radius = AppConstants.borderRadius
-//           ..backgroundColor = AppColors.neutralColor100
-//           ..indicatorColor = AppColors.primaryColor900
-//           ..textColor = Colors.black
-//           ..maskColor = Colors.black.withValues(alpha: 0.5)
-//           ..progressColor = AppColors.primaryColor900
-//           ..userInteractions = false;
-
-//         if (total != -1) {
-//           double progress = received / total;
-//           EasyLoading.showProgress(
-//             maskType: EasyLoadingMaskType.clear,
-//             progress,
-//             status: 'Downloading... ${(progress * 100).toStringAsFixed(0)}%',
-//           );
-//         }
-//       },
-//     );
-
-//     EasyLoading.dismiss();
-//     EasyLoading.showSuccess(
-//       "Downloaded Successfully",
-//     );
-//     debugPrint("File downloaded to: $savePath");
-//   } catch (e) {
-//     EasyLoading.dismiss();
-//     EasyLoading.showError("Download failed");
-//     debugPrint("Download failed: $e");
-//   }
-// }
-
-// import 'dart:io';
-// import 'package:dio/dio.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_easyloading/flutter_easyloading.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:permission_handler/permission_handler.dart';
-// import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
-// import 'package:truee_balance_app/core/themes/app_colors.dart';
-// import 'package:truee_balance_app/core/utils/app_constants.dart';
-
-// Future<void> downloadPdfFile(String url, String fileName) async {
-//   try {
-//     if (Platform.isAndroid) {
-//       // Android 11+ يحتاج manageExternalStorage
-//       if (await Permission.manageExternalStorage.isDenied) {
-//         var status = await Permission.manageExternalStorage.request();
-//         if (!status.isGranted) {
-//           EasyLoading.showError("Permission denied");
-//           return;
-//         }
-//       }
-//     }
-
-//     Directory? downloadsDir = await DownloadsPathProvider.downloadsDirectory;
-//     if (downloadsDir == null) {
-//       EasyLoading.showError("Downloads folder not found");
-//       return;
-//     }
-
-//     String savePath = "${downloadsDir.path}/$fileName";
-
-//     Dio dio = Dio();
-//     await dio.download(
-//       url,
-//       savePath,
-//       onReceiveProgress: (received, total) {
-//         EasyLoading.instance
-//           ..indicatorType = EasyLoadingIndicatorType.fadingCircle
-//           ..loadingStyle = EasyLoadingStyle.custom
-//           ..indicatorSize = 50.sp
-//           ..radius = AppConstants.borderRadius
-//           ..backgroundColor = AppColors.neutralColor100
-//           ..indicatorColor = AppColors.primaryColor900
-//           ..textColor = Colors.black
-//           ..maskColor = Colors.black.withOpacity(0.5)
-//           ..progressColor = AppColors.primaryColor900
-//           ..userInteractions = false;
-
-//         if (total != -1) {
-//           double progress = received / total;
-//           EasyLoading.showProgress(
-//             progress,
-//             status: 'Downloading... ${(progress * 100).toStringAsFixed(0)}%',
-//             maskType: EasyLoadingMaskType.clear,
-//           );
-//         }
-//       },
-//     );
-
-//     EasyLoading.dismiss();
-//     EasyLoading.showSuccess("Saved to Downloads");
-//     debugPrint("File downloaded to: $savePath");
-//   } catch (e) {
-//     EasyLoading.dismiss();
-//     EasyLoading.showError("Download failed");
-//     debugPrint("Download failed: $e");
-//   }
-// }
 import 'dart:io';
 
-import 'package:dio/dio.dart';
-import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:truee_balance_app/core/themes/app_colors.dart';
-import 'package:truee_balance_app/core/utils/app_constants.dart';
-
-// Future<void> downloadPdfFile(String url, String fileName) async {
-//   try {
-//     if (Platform.isAndroid) {
-//       // Android 11 وأعلى
-//       if (await Permission.manageExternalStorage.isDenied) {
-//         var status = await Permission.manageExternalStorage.request();
-//         if (!status.isGranted) {
-//           EasyLoading.showError("Storage permission denied");
-//           return;
-//         }
-//       }
-
-//       // Android 10 وأقل
-//       if (await Permission.storage.isDenied) {
-//         var status = await Permission.storage.request();
-//         if (!status.isGranted) {
-//           EasyLoading.showError("Storage permission denied");
-//           return;
-//         }
-//       }
-//     }
-
-//     Directory? downloadsDir = await DownloadsPathProvider.downloadsDirectory;
-//     if (downloadsDir == null) {
-//       EasyLoading.showError("Downloads folder not found");
-//       return;
-//     }
-
-//     if (!downloadsDir.existsSync()) {
-//       downloadsDir.createSync(recursive: true);
-//     }
-
-//     String savePath = "${downloadsDir.path}/$fileName";
-
-//     Dio dio = Dio();
-//     await dio.download(
-//       url,
-//       savePath,
-//       onReceiveProgress: (received, total) {
-//         EasyLoading.instance
-//           ..indicatorType = EasyLoadingIndicatorType.fadingCircle
-//           ..loadingStyle = EasyLoadingStyle.custom
-//           ..indicatorSize = 50.sp
-//           ..radius = AppConstants.borderRadius
-//           ..backgroundColor = AppColors.neutralColor100
-//           ..indicatorColor = AppColors.primaryColor900
-//           ..textColor = Colors.black
-//           ..maskColor = Colors.black.withOpacity(0.5)
-//           ..progressColor = AppColors.primaryColor900
-//           ..userInteractions = false;
-
-//         if (total != -1) {
-//           double progress = received / total;
-//           EasyLoading.showProgress(
-//             progress,
-//             status: 'Downloading... ${(progress * 100).toStringAsFixed(0)}%',
-//             maskType: EasyLoadingMaskType.clear,
-//           );
-//         }
-//       },
-//     );
-
-//     EasyLoading.dismiss();
-//     EasyLoading.showSuccess("Saved to Downloads");
-//     debugPrint("✅ File downloaded to: $savePath");
-//   } catch (e) {
-//     EasyLoading.dismiss();
-//     EasyLoading.showError("Download failed");
-//     debugPrint("❌ Download failed: $e");
-//   }
-// }
-import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:truee_balance_app/core/themes/app_colors.dart';
 import 'package:truee_balance_app/core/utils/app_constants.dart';
 
 Future<void> downloadPdfFile(String url, String fileName) async {
   try {
+    debugPrint("Downloading from: $url");  
+    Directory? dir;
     if (Platform.isAndroid) {
-      // Android 10 وأقل
-      if (await Permission.storage.isDenied) {
-        var status = await Permission.storage.request();
-        if (!status.isGranted) {
-          EasyLoading.showError("Storage permission denied");
-          return;
-        }
+      dir = Directory('/storage/emulated/0/Download');
+      if (!dir.existsSync()) {
+        dir = await getExternalStorageDirectory();
       }
-    }
-
-    // 📂 تحديد مكان الحفظ
-    Directory? downloadsDir;
-    if (Platform.isAndroid) {
-      downloadsDir = Directory("/storage/emulated/0/Download");
     } else {
-      downloadsDir = await getApplicationDocumentsDirectory();
+      dir = await getApplicationDocumentsDirectory();
     }
 
-    if (!downloadsDir.existsSync()) {
-      downloadsDir.createSync(recursive: true);
-    }
-
-    String savePath = "${downloadsDir.path}/$fileName";
+    String savePath = "${dir!.path}/$fileName";
 
     Dio dio = Dio();
     await dio.download(
       url,
       savePath,
       onReceiveProgress: (received, total) {
+        EasyLoading.instance
+          ..indicatorType = EasyLoadingIndicatorType.fadingCircle
+          ..loadingStyle = EasyLoadingStyle.custom
+          ..indicatorSize = 50.sp
+          ..radius = AppConstants.borderRadius
+          ..backgroundColor = AppColors.neutralColor100
+          ..indicatorColor = AppColors.primaryColor900
+          ..textColor = Colors.black
+          ..maskColor = Colors.black.withValues(alpha: 0.5)
+          ..progressColor = AppColors.primaryColor900
+          ..userInteractions = false;
+
         if (total != -1) {
           double progress = received / total;
           EasyLoading.showProgress(
@@ -403,10 +123,10 @@ Future<void> downloadPdfFile(String url, String fileName) async {
 
     EasyLoading.dismiss();
     EasyLoading.showSuccess("Saved to Downloads");
-    debugPrint("✅ File downloaded to: $savePath");
+    debugPrint("File downloaded to: $savePath");
   } catch (e) {
     EasyLoading.dismiss();
     EasyLoading.showError("Download failed");
-    debugPrint("❌ Download failed: $e");
+    debugPrint("Download failed: $e");
   }
 }
