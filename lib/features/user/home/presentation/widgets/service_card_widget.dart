7d18bf34534a4db002d3ff7a2e825150c1c6316e
Ahmed Adel <aadel@tanfeethi.com.sa>
2025-09-22 13:13:22 +0300

    Edit image in our services in home screen & all services screeen

--- lib/features/user/home/presentation/widgets/service_card_widget.dart
+++ lib/features/user/home/presentation/widgets/service_card_widget.dart
@@ -1,9 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:truee_balance_app/core/themes/app_colors.dart';
+import 'package:truee_balance_app/core/themes/hex_colors.dart';
 import 'package:truee_balance_app/core/themes/text_colors.dart';
 import 'package:truee_balance_app/core/utils/app_constants.dart';
-import 'package:truee_balance_app/core/widgets/images/cache_network_image/image_widget.dart';
 
 class ServiceCardWidget extends StatelessWidget {
   const ServiceCardWidget({
@@ -18,6 +19,7 @@ class ServiceCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(12.sp),
       decoration: BoxDecoration(
         color: AppColors.neutralColor100,
         borderRadius: BorderRadius.circular(AppConstants.borderRadius),
@@ -25,32 +27,44 @@ class ServiceCardWidget extends StatelessWidget {
           width: 1.w,
           color: const Color(0xFF030303).withValues(alpha: 0.1),
         ),
+        boxShadow: [
+          BoxShadow(
+            color: hexToColor('#0062FF').withAlpha(5),
+            blurRadius: 50.r,
+            spreadRadius: 0,
+            offset: Offset(10.w, 10.h),
+          ),
+        ],
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(4.r),
-                child: CacheNetworkImagesWidget(
-                  image: image?.isNotEmpty == true
-                      ? image!
-                      : 'assets/images/png/placeholder.png',
+      child: Column(
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(4.r),
+              child: CachedNetworkImage(
+                // haveBorder: true,
+                // isFile: false,
+                imageUrl: image?.isNotEmpty == true
+                    ? image!
+                    : 'assets/images/png/placeholder.png',
+                errorWidget: (context, url, error) => Icon(
+                  Icons.error,
+                  color: AppColors.neutralColor1000,
+                  size: 44.sp,
                 ),
               ),
             ),
-            8.verticalSpace,
-            Text(
-              title ?? '---',
-              style: Styles.captionRegular.copyWith(
-                color: AppColors.neutralColor1000,
-              ),
-              overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.center,
+          ),
+          8.verticalSpace,
+          Text(
+            title ?? '---',
+            style: Styles.captionRegular.copyWith(
+              color: AppColors.neutralColor1000,
             ),
-          ],
-        ),
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
